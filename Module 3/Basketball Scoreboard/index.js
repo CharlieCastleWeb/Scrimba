dbac0eb809434cf2cbb0a98d9eeea78bb0e02eef
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2023-06-27 20:24:56 +0200

    Create HTML structure and styles

---
+++ Module 3/Basketball Scoreboard/index.js
@@ -0,0 +1,8 @@
+
+const homePlusOne = document.querySelector("#home-plus-one");
+const homePlusTwo = document.querySelector("#home-plus-two");
+const homePlusThree = document.querySelector("#home-plus-three");
+const guestPlusOne = document.querySelector("#guest-plus-one");
+const guestPlusTwo = document.querySelector("#guest-plus-two");
+const guestPlusThree = document.querySelector("#guest-plus-three");
+
