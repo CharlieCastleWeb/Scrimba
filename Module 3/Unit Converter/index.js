9ebbc83a5e785587e27676bd6aa2a326f7a2749b
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2023-07-10 06:54:51 +0200

    Fix minor bugs

--- Module 3/Unit Converter/index.js
+++ Module 3/Unit Converter/index.js
@@ -1,4 +1,5 @@
 
+
 const input                  = document.querySelector("input");
 const convertBtn             = document.querySelector("button");
 const userNumbersDisplay     = document.querySelectorAll(".user-number");
