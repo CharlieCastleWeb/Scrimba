698b08313d8a488e458035b59c3cc35ce5fa077a
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2023-07-19 19:02:29 +0200

    Send To Pager - Complete Project

--- Module 3/sendToPager/index.js
+++ Module 3/sendToPager/index.js
@@ -4,9 +4,11 @@ const phoneDisplay = document.querySelector("#phone-display");
 const phoneKeys    = document.querySelectorAll(".key");
 const resetBtn     = document.querySelector("#reset-btn");
 const sendBtn      = document.querySelector("#send-btn");
+const pagerSound   = new Audio("./assets/pager.wav");
 
 let message = "";
 
+// Add event listener to keys
 phoneKeys.forEach( (key) => {
     key.addEventListener("click", () => {
         message += key.value;
@@ -14,13 +16,23 @@ phoneKeys.forEach( (key) => {
     });
 });
 
+// Add event listener to reset button
 resetBtn.addEventListener("click", () => {
     message = "";
     phoneDisplay.textContent = message;
 });
 
+// Add event listener to reset send
 sendBtn.addEventListener("click", () => {
-    pagerDisplay.textContent = message;
-    message = ""
-    phoneDisplay.textContent = message;
+    if (message != "") {
+        setTimeout( () => {
+            pagerDisplay.textContent = message;
+            message = ""
+            pagerSound.play();
+        },1000);
+        phoneDisplay.textContent = "sending...";
+        setTimeout( () => {
+            phoneDisplay.textContent = message;
+        }, 1000);
+    }
 });
