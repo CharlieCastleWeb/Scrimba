cc6f69060e31d9073f4be13eeb5757296e762ccf
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2023-09-11 16:24:55 +0200

    M6-LearningJournal-CompleteProject

---
+++ Module 6/LearningJournal/data.js
@@ -0,0 +1,85 @@
+const recentPosts = [
+    {
+        url: "#",
+        imgUrl: "./assets/images/article-image-03.png",
+        imgAlt: "Computer setup",
+        postDate: "JULY 23, 2022",
+        postTitle: "Blog one",
+        postContent:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    {
+        url: "#",
+        imgUrl: "./assets/images/article-image-02.png",
+        imgAlt: "Lightbulb",
+        postDate: "JULY 23, 2022",
+        postTitle: "Blog two",
+        postContent:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    {
+        url: "#",
+        imgUrl: "./assets/images/article-image-01.png",
+        imgAlt: "Computer setup",
+        postDate: "JULY 23, 2022",
+        postTitle: "Blog three",
+        postContent:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    {
+        url: "#",
+        imgUrl: "./assets/images/article-image-04.png",
+        imgAlt: "Web developement screen",
+        postDate: "JULY 23, 2022",
+        postTitle: "Blog four",
+        postContent:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    {
+        url: "#",
+        imgUrl: "./assets/images/article-image-05.png",
+        imgAlt: "Girl dancing in the beach",
+        postDate: "JULY 23, 2022",
+        postTitle: "Blog five",
+        postContent:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    {
+        url: "#",
+        imgUrl: "./assets/images/article-image-06.png",
+        imgAlt: "Purple background",
+        postDate: "JULY 23, 2022",
+        postTitle: "Blog six",
+        postContent:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    {
+        url: "#",
+        imgUrl: "./assets/images/article-image-03.png",
+        imgAlt: "Computer setup",
+        postDate: "JULY 23, 2022",
+        postTitle: "Blog seven",
+        postContent:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    {
+        url: "#",
+        imgUrl: "./assets/images/article-image-02.png",
+        imgAlt: "Lightbulb",
+        postDate: "JULY 23, 2022",
+        postTitle: "Blog eight",
+        postContent:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    {
+        url: "#",
+        imgUrl: "./assets/images/article-image-01.png",
+        imgAlt: "Computer setup",
+        postDate: "JULY 23, 2022",
+        postTitle: "Blog nine",
+        postContent:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+];
+
+export default recentPosts;
---
+++ Module 6/LearningJournal/index.js
@@ -0,0 +1,50 @@
+import recentPosts from "./data.js";
+
+const postsDisplay = document.querySelector("#posts");
+const viewMoreBtn = document.querySelector("#view-more-btn");
+
+let recentPostsNum = 0;
+
+viewMoreBtn.addEventListener("click", () => {
+    if (recentPostsNum < recentPosts.length) {
+        displayPosts(recentPostsNum);
+    }
+    
+    if (recentPostsNum === recentPosts.length) {
+        viewMoreBtn.parentElement.removeChild(viewMoreBtn);
+    }
+});
+
+const displayPosts = (num) => {
+    for (let i = num; i < num + 3; i++) {
+        postsDisplay.innerHTML += displayPost(recentPosts[i]);
+    }
+    recentPostsNum += 3;
+}
+
+const displayPost = ({
+    url,
+    imgUrl,
+    imgAlt,
+    postDate,
+    postTitle,
+    postContent,
+}) => {
+    const template = `
+        <div class="post">
+            <a href="${url}">
+                <img
+                    src="${imgUrl}"
+                    alt="${imgAlt}"
+                />
+                <p class="post-date">${postDate}</p>
+                <h3>${postTitle}</h3>
+                <p class="post-content">${postContent}</p>
+            </a>
+        </div>
+        `;
+    return template;
+};
+
+displayPosts(recentPostsNum);
+
